bae61b4c7d9c8ebc8b88401f5077bc7e606cae70
junn <60811775+toodox@users.noreply.github.com>
2020-08-17 01:34:04 +0900

    finished!

---
+++ js/weather.js
@@ -0,0 +1,62 @@
+const weather = document.querySelector(".js-weather");
+const weatherTemp = weather.querySelector(".weather__temp");
+const weatherLoca = weather.querySelector(".weather__loca");
+
+const API_KEY = "bc01573f20259de06a948970c4bccaf2";
+const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weatherTemp.innerText = `🌈${temperature}℃`;
+      weatherLoca.innerText = `📍${place}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  const coordsObj = {
+    latitude,
+    longitude,
+    //구조체 이름과 그 속성값의 이름이 같을 때는 이렇게 선언해도 무방
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log("Cant access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    //getWeather
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+  }
+}
+
+function init() {
+  loadCoords();
+}
+init();
